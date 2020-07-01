d70e306bc4430e34092c39cf003327c03a2b532c
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-01 16:55:11 -0500

    componente para aprender a manejar los customs hooks

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const HelloWorld = () => {
+
+    const [ active, setActive ] = useState(true);
+
+    const handleClick = () => {
+        setActive(!active);
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>Ocultar</button>
+            {active &&
+                <h1>Hola Mundo 2</h1>
+            }            
+        </div>
+    );
+}
+
+export default HelloWorld;
+
+// import React, { Component } from 'react';
+
+// class HelloWorld extends Component{
+
+//     state = {
+//         active: true,
+//     }
+
+//     handleClick = () => {  
+//         this.setState({
+//             active: !this.state.active,
+//         });
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//                 <button onClick={ this.handleClick } >Ocultar</button>
+//                 { this.state.active && 
+//                     <h1>Hola Mundo</h1>
+//                 }
+//             </div>
+//         );
+//     }
+// }
+
+// export default HelloWorld;
