9322c34095ed3c84976b293fafed4e3110de2300
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-01 16:14:30 -0500

    actualizados education, certificates, experience

---
+++ src/components/Certificates.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Certificates = () => {
+    <div className="Certificates">
+        <div className="Certificates-container">
+            <div className="Certificates-item">
+                <h3>FrontEnd</h3>
+                <p>FrontEnd Developer</p>
+            </div>
+        </div>
+    </div>
+}
+
+export default Certificates;
---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Experience = () => {
+    <div className="Experience">
+        <div className="Experience-container">
+            <div className="Experience-item">
+                <h3>FrontEnd</h3>
+                <p>Desarrollador FrontEnd Especificalo</p>
+            </div>
+        </div>
+    </div>
+}
+
+export default Experience;
