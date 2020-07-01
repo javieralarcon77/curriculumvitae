07e3894c797b83a224f2a5d77b1388310f63b3c4
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-01 16:08:36 -0500

    actualizado about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const About = () => (
-    <h1>Hola Mundo Devs</h1>
+    <div className="About">
+        <div className="About-container">
+            <div className="About-avatar">
+                <figure>
+                    <img src="/" alt="/" />
+                </figure>
+            </div>
+            <div className="About-name">
+                <h2>Javier Alarcon</h2>
+            </div>
+            <div className="About-profesion">
+                <p>FrontEnd Developer</p>
+            </div>
+            <div className="About-desc">
+                <p>Soy desarrollador web y me encanten los pugs</p>
+            </div>
+            <div className="About-location">
+                <p>Medellín - Colombia</p>
+            </div>
+            <div className="About-social">
+                Social
+            </div>
+        </div>
+    </div>
 );
 
 export default About;
