0d04531e8ee1b0b10667bd5d14583da8bb2e5ba5
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-04 15:11:06 -0500

    Animaciones con Styled Components para las barras de skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const SkillsContainer = styled.div`
     display:grid;
@@ -18,6 +18,10 @@ const SkillsLine = styled.div`
     border-radius: 5px;
     background: #F8BBD0;
 `;
+const move = keyframes`
+    0% { background-position: 0 0; }
+    100% { background-position: 50px 50px; }
+`;
 
 const SkillsSpan = styled.span`
     display:block;
@@ -28,8 +32,22 @@ const SkillsSpan = styled.span`
     overflow: hidden;
     position:relative;
     box-shadow: inset 0 2px 9px rgba(255,255,255, 0.3), inset 0 -2px 6px rgba(0,0,0, 0.4);
-    
+    &:after{
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right:0;
+        background-image: linear-gradient(-45deg, rgba(255,255,255,0.2) 25%, transparent 25%, transparent 50%, rgba(255,255,255,0.2) 50%, rgba(255,255,255,0.2) 75%, transparent 75%, transparent);
+        z-index:1;
+        background-size: 50px 50px;
+        animation: ${ move } 4s linear infinite;
+        overflow:hidden;
+    }
 `;
+
+
 
 
 const Skills = props => (
