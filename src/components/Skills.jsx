6e87c28c13de117551606f889df7067f286f98b8
Javier Alarcon <alarcon.vt@gmail.com>
2020-07-04 14:58:46 -0500

    Creando las barras de progreso para las skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const SkillsContainer = styled.div`
+    display:grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 2rem;
+    grid-row-gap: 0.5em;
+`;
+
+const SkillsH5 = styled.h5`
+    margin: .5em 0;
+`;
+
+const SkillsLine = styled.div`
+    height: 8px;
+    position:relative;
+    border-radius: 5px;
+    background: #F8BBD0;
+`;
+
+const SkillsSpan = styled.span`
+    display:block;
+    height: 100%;
+    width: ${ props => props.width };
+    border-radius: 8px;
+    background-color: #c2185b;
+    overflow: hidden;
+    position:relative;
+    box-shadow: inset 0 2px 9px rgba(255,255,255, 0.3), inset 0 -2px 6px rgba(0,0,0, 0.4);
+    
+`;
+
 
 const Skills = props => (
     <div className="Skills">
-        <div className="Skills-container">
+        <SkillsContainer>
             {
                 props.data.map((skill,index) => (
                     <div className="Skills-item" key={ `Skill-${index}` }>
-                        <h5>{ skill.name }</h5>
-                        <div className="Skills-line">
-                            <span>{ skill.percentage }</span>
-                        </div>
+                        <SkillsH5>{ skill.name }</SkillsH5>
+                        <SkillsLine>
+                            <SkillsSpan width={skill.percentage} />
+                        </SkillsLine>
                     </div>
                 ))
 
-            }
-            
-        </div>
+            }            
+        </SkillsContainer>
     </div>
 )
 
