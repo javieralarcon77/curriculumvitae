949da86f6d95fc459aa0579408d35d940b9bb4cc
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2022-03-04 14:40:30 -0500

    feat: update get data for api resourse

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -1,16 +1,27 @@
-import { useState, useEffect } from 'react';
-import data from './datos.json';
+import { useState, useEffect } from "react";
+import data from "./datos.json";
 
-const api = 'https://us-central1-gndx-cv.cloudfunctions.net/me';
+const api = "https://javier.especificalo.com/datos.json";
 
-const useGetData = () =>{
-    const [ mydata, setData ] = useState([]);
+const useGetData = () => {
+  const [mydata, setData] = useState([]);
 
-    useEffect(()=>{
+  useEffect(() => {
+    fetch(api, {
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => {
+        console.log("get data by api");
+        setData(resp);
+      })
+      .catch((e) => {
         setData(data);
-    }, []);
+      });
+  }, []);
 
-    return mydata;
-}
+  return mydata;
+};
 
 export default useGetData;
